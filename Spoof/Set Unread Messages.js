e739995359701caadc71a8f908fa90a377d552a1
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2023-04-11 14:28:46 -0400

    Delete Spoof directory
    useless

--- Spoof/Set Unread Messages.js
+++
@@ -1,2 +0,0 @@
-var msgs = parseInt(prompt("Enter unread count:", "69"));
-document.getElementsByClassName('menu-item__badge')[2].innerHTML = '<span class="ergWt_bGBk">"One unread message."</span><span aria-hidden="true">' + msgs + '</span>';
